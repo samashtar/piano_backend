f3b60f24ff4e256dcab45bdb67b471a6803b81c9
samashtar <sam.ashtar@flatironschool.com>
2018-11-08 10:51:14 -0500

    added notes to db

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Models/Resources:
   - UserName
   - PassWord
 - Songs
+
   - Name
   - Length
   - User_Id
+
+- Notes
+  - Name - string
+    A, A#, B, C, C#, D, D#, E, F, F#, G, G#
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_212909) do
+ActiveRecord::Schema.define(version: 2018_11_08_142956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
